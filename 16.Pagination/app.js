a41cab4970a728615420e90ca29d35b933adbc88
moise <mose.ns@hotmail.com>
2021-01-26 14:26:10 +0100

    Setup project fetch followers

---
+++ 16.Pagination/app.js
@@ -0,0 +1,14 @@
+import fetchFollowers from './fetchFollowers.js'
+import displayFollowers from './displayFollowers.js'
+import paginate from './paginate.js'
+import displayButtons from './displayButtons.js'
+
+
+const init = async () => {
+  const followers = await fetchFollowers();
+  console.log(followers);
+}
+
+window.addEventListener('load', init);
+
+
